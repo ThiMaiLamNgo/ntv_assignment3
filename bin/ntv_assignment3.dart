5e7375d206c6a74d861bed4e0cf4b7399302df31
ThiMaiLamNgo <thimailamngo@gmail.com>
2021-10-07 22:00:54 +0000

    Assignment 3_3

--- bin/ntv_assignment3.dart
+++ bin/ntv_assignment3.dart
@@ -1,5 +1,22 @@
 import 'dart:io';
 import 'dart:math';
+
+// ASSIGNMENT 3_3:
+void main() {
+  print('Enter five numbers, please!');
+  int counter = 0;
+  int sum = 0;
+  while(counter < 5){
+    print('Enter a number: ');
+    int number = int.parse(stdin.readLineSync());
+    counter ++;
+    sum += number;
+  }
+  print('Sum of the numbers is: $sum');
+  print('Average of the numbers is: ${sum / counter}');
+}
+
+/* ASSIGNMENT 3-2:
 void main() {
   print('Enter a number: ');
   int number = int.parse(stdin.readLineSync());
@@ -7,6 +24,7 @@ void main() {
     print('$number * $i = ${number * i}');
   }
 }
+*/
 
 /*
 // Assignment 3_ 1:
