2042092491a638fccf88c0a7116a8d3376d8dbbc
ThiMaiLamNgo <thimailamngo@gmail.com>
2021-10-07 21:04:15 +0000

    change

--- bin/ntv_assignment3.dart
+++ bin/ntv_assignment3.dart
@@ -1,3 +1,12 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  int sum = 0;
+  for (int number = 1; number <= 10; number++) {
+    print(number);
+    sum = sum + number;
+  }
+  print('Sum of first ten number is: $sum');
 }
+
