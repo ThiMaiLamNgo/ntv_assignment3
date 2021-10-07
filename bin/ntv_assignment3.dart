0952fe31164c02da5353bf8342bd62054a622f19
ThiMaiLamNgo <thimailamngo@gmail.com>
2021-10-07 21:36:17 +0000

    assignment3_1

--- bin/ntv_assignment3.dart
+++ bin/ntv_assignment3.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 import 'dart:math';
-
+// Assignment 3_ 1:
 void main() {
   int sum = 0;
   for (int number = 1; number <= 10; number++) {
@@ -12,4 +12,16 @@ void main() {
   }
   print('Sum of first ten numbers is: $sum');
 }
-
+/*Answer:
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+Sum of first ten numbers is: 55
+*/
